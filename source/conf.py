6888064e48ee6d083b96bffa0772bd642715c786
Balaji Thope Janakiram <Balaji_Janakiram@Dell.com>
2018-05-07 16:18:18 +0530

    Edited html theme for readthedocs   Changes to be committed:  	modified:   source/conf.py

--- source/conf.py
+++ source/conf.py
@@ -74,7 +74,16 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+
+# check for local or RTD build
+import os
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+else:
+    html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
